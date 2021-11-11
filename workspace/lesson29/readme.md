dd77cf5e2f75260ab7ac3773334eb2ebbb893c8b
jincheng9 <perfume0607@gmail.com>
2021-11-11 14:09:49 +0800

    update select

--- workspace/lesson29/readme.md
+++ workspace/lesson29/readme.md
@@ -33,13 +33,41 @@ func d() {
 }
 ```
 
+语法上和[switch](../lesson6)的一些区别：
 
+* select关键字和后面的{ 之间，不能有表达式或者语句。
+* 没有fallthrough语句
+* 每个case关键字后面跟的必须是channel的发送或者接收操作
+* 允许多个case分支使用相同的channel，case分支后的语句甚至可以重复
 
 ## 示例
 
+```go
+func b() {
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+	go func() {
+		for i:=0; i<10; i++ {
+			ch1 <- i
+			ch2 <- i
+		}
+	}()
+	for i := 0; i < 10; i++ {
+		select {
+		case x := <-ch1:
+			fmt.Printf("receive %d from channel 1\n", x)
+		case y := <-ch2:
+			fmt.Printf("receive %d from channel 2\n", y)
+		}
+	}
+}
+```
 
+上面的示例里，select语句会for循环执行10次，每次select语句都会随机从channel1或者channel2里接收一个值并打印。可以看把[示例代码](./select.go)下载本地运行看结果。
 
 ## References
 
 * https://golang.google.cn/ref/spec#Select_statements
 * https://gobyexample.com/select
+* https://go101.org/article/channel.html
+* https://medium.com/a-journey-with-go/go-ordering-in-select-statements-fd0ff80fd8d6
--- workspace/lesson29/select.go
+++ workspace/lesson29/select.go
@@ -6,43 +6,52 @@ import (
 )
 
 func a() {
-	var c1, c2, c3 chan int = make(chan int, 1), make(chan int, 1), make(chan int, 1)
+	var c1, c2, c3 chan int = make(chan int), make(chan int), make(chan int)
 	var i1, i2 int
 	go func() {
 		c1 <- 10
 		i1 = <-c2
 	}()
-	time.Sleep(time.Second)
-	select {
-	case c2 <- i2:
-		print("sent ", i2, " to c2\n")
-	case i1 = <-c1:
-		fmt.Print("received ", i1, " from c1\n")
-	case i3, ok := (<-c3): // same as: i3, ok := <-c3
-		if ok {
-			print("received ", i3, " from c3\n")
-		} else {
-			print("c3 is closed\n")
+	for {
+		select {
+		case c2 <- i2:
+			print("sent ", i2, " to c2\n")
+		case i1 = <-c1:
+			fmt.Print("received ", i1, " from c1\n")
+		case i3, ok := (<-c3): // same as: i3, ok := <-c3
+			if ok {
+				print("received ", i3, " from c3\n")
+			} else {
+				print("c3 is closed\n")
+			}
+		default:
+			print("no communication\n")
 		}
-	// case a[f()] = <-c4:
-	// same as:
-	// case t := <-c4
-	//	a[f()] = t
-	default:
-		print("no communication\n")
+		time.Sleep(2*time.Second)
 	}
 }
 
+
 func b() {
-	var ch chan int
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
 	go func() {
-		fmt.Println("b1")
-		ch <- 10
-		fmt.Println("b2")
+		for i:=0; i<10; i++ {
+			ch1 <- i
+			ch2 <- i
+		}
 	}()
-	time.Sleep(2 * time.Second)
-	fmt.Println("func b")
+	for i := 0; i < 10; i++ {
+		select {
+		case x := <-ch1:
+			fmt.Printf("receive %d from channel 1\n", x)
+		case y := <-ch2:
+			fmt.Printf("receive %d from channel 2\n", y)
+		}
+	}
 }
+
 func main() {
+	//a()
 	b()
 }
