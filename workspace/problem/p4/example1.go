19fd9fde4ad06dc8409ea3b198b14311ac8f0d71
jincheng9 <perfume0607@gmail.com>
2021-11-25 22:29:09 +0800

    update

--- workspace/problem/p4/example1.go
+++ workspace/problem/p4/example1.go
@@ -14,11 +14,10 @@ type A struct {
 func main() {
 	var a bytes.Buffer
 	fmt.Println(a)
-	var b[][5]int
-	b[0] = [5]int{1}
-	fmt.Println(b == nil)
 
 	c := []int{}
 	fmt.Println(c == nil)
 
+	d := new([]int)
+	fmt.Println(d)
 }
