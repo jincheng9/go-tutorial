89a51f1d766437fa4369833af51975056193a912
jincheng9 <perfume0607@gmail.com>
2021-12-27 15:27:35 +0800

    update

--- workspace/test/example7.go
+++ workspace/test/example7.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
+func test() {
+	s := []int{1, 2}
+	fmt.Printf("%p %p %p\n", &s, s, &s[0])
+	s = make([]int, 2, 3)
+	fmt.Printf("%p %p %p\n", &s, s, &s[0])
+}
 func main() {
+	test()
 	a := [...]int{0, 1, 2, 3}
 	fmt.Printf("%T %d %d\n", a, len(a), cap(a))
 	x := a[:1]
