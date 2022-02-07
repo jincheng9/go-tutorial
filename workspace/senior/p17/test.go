0cf493d6c07ec4a265bc5e96a994d81cfa1df18a
jincheng9 <perfume0607@gmail.com>
2022-02-07 11:24:12 +0800

    update

--- workspace/senior/p17/test.go
+++ workspace/senior/p17/test.go
@@ -7,7 +7,7 @@ import (
 )
 
 // return the min value
-func min[T constraints.Ordered](a, b T /*"T constraints.Ordered"*/) T {
+func min[T constraints.Ordered](a, b T) T {
 	fmt.Printf("%T ", a)
 	if a < b {
 		return a
