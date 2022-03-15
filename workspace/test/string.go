2994cff095f098dae9851d5f0392485fee1095b2
jincheng9 <perfume0607@gmail.com>
2022-03-15 21:51:50 +0800

    update

--- workspace/test/string.go
+++ workspace/test/string.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println(upperStr, result)
 
 	a := string(97) // 把Unicode值转成字符串, "a"
-	fmt.Println(a == "a")
+	fmt.Println(a, a == "a")
 
 	b := string(0x5403) // 0x5403对应的字符是中文"吃"
 	fmt.Println(b)
