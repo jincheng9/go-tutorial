de111f62ba9bd325f8239d70f98febef34663a25
jincheng9 <perfume0607@gmail.com>
2022-03-15 21:50:06 +0800

    update

--- workspace/test/string.go
+++ workspace/test/string.go
@@ -11,6 +11,9 @@ func main() {
 	result := strings.HasPrefix(upperStr, "LIVE")
 	fmt.Println(upperStr, result)
 
-	a := string(97) // 根据ASCII码值转成对应的字符, "a"
+	a := string(97) // 把Unicode值转成字符串, "a"
 	fmt.Println(a == "a")
+
+	b := string(0x5403) // 0x5403对应的字符是中文"吃"
+	fmt.Println(b)
 }
