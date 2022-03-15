f1e6fc99d3aef8056c0dbcbf1557b0f67b83432e
jincheng9 <perfume0607@gmail.com>
2022-03-15 21:45:47 +0800

    update

--- workspace/test/string.go
+++ workspace/test/string.go
@@ -10,4 +10,7 @@ func main() {
 	upperStr := strings.ToUpper(str)
 	result := strings.HasPrefix(upperStr, "LIVE")
 	fmt.Println(upperStr, result)
+
+	a := string(97) // 根据ASCII码值转成对应的字符, "a"
+	fmt.Println(a == "a")
 }
