ebe81df2b559d74acc9c8d94463f92e9017073b0
jincheng9 <perfume0607@gmail.com>
2022-04-26 17:34:42 +0800

    update

--- workspace/test/operator/main.go
+++ workspace/test/operator/main.go
@@ -4,7 +4,17 @@ import (
 	"fmt"
 )
 
+import "os"
+
+var _s = test()
+
+func test() string {
+	return "test"
+}
+
 func main() {
 	result := false && true || true
 	fmt.Println(result)
+	fmt.Println(_s)
+	fmt.Println(os.Getwd())
 }
