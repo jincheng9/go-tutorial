647b8f46d5cec3784f7088c3824c62579c0b0281
jincheng9 <perfume0607@gmail.com>
2023-03-25 21:53:25 +0800

    update

---
+++ workspace/senior/p36/code/go.mod
@@ -0,0 +1,5 @@
+module chatgpt.com/test
+
+go 1.16
+
+require github.com/sashabaranov/go-openai v1.5.2
---
+++ workspace/senior/p36/code/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	client := openai.NewClient("your token")
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
